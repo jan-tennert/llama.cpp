7192a7ddc841de60d68b7b3d529abe4e26282830
TheRealJan <jan.m.tennert@gmail.com>
2023-09-12 18:41:37 +0200

    update local file

--- build_hipblas.bat
+++ build_hipblas.bat
@@ -3,5 +3,5 @@ set HIP_PATH=D:\Programms\AMD\Rocm\5.5
 set CC=D:\Programms\AMD\Rocm\5.5\bin\clang.exe
 set CXX=D:\Programms\AMD\Rocm\5.5\bin\clang++.exe
 set VERBOSE=1
-cmake -B build -G "Ninja" -DLLAMA_HIPBLAS=ON -DAMDGPU_TARGETS=gfx1012 -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
+cmake -B build -G "Ninja" -DLLAMA_HIPBLAS=ON -DAMDGPU_TARGETS=gfx1012 -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DLLAMA_STATIC=Off
 cmake --build build
